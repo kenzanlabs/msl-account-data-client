ee2eb56b7e0b06b829340fe5d0267b5a8d05838e
anram88 <aramirez@kenzanmedia.com>
2016-01-03 12:46:07 -0600

    New awesome Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# MSL account data client
+
+## Overview
+Data Client Layer
+Simplification to a traditional edge/middle architecture, this project uses a edge/data client architecture instead.
+The data clients are jars, each containing all the methods and DAOs for accessing all the tables within a Cassandra cluster.
+To enhance scalability and configuration flexibility, the Cassandra tables are split into three independent clusters: account, catalog, and rating.
+Each of these clusters has a data client jar dedicated to accessing it: account-data-client, catalog-data-client, and rating-data-client, respectively.
+
+So a microservice that needs to access Cassandra data will include one or more of the data client jars.
+
+| Table           | Method  |
+|:-------------:| -----:|
+| **users** | Observable<Void> addOrUpdateUser(UserDao) |
+| | Observable<UserDao> getUser(UUID userUuid) |
+| | Observable<Void> deleteUser(UUID userUuid) |
+| **songs_by_user** | Observable<Void> addOrUpdateSongsByUser(SongsByUserDao) |
+| | Observable<SongsByUserDao> getSongsByUser(UUID userId, String favoritesTimestamp, UUID songUuid) |
+| | Observable<ResultSet> getSongsByUser(UUID userId, Optional<String >favoritesTimestamp, Optional<Integer> limit) |
+| | Observable<Result<SongsByUserDao>> map(Observable<ResultSet>) |
+| | Observable<Void> deleteSongsByUser(UUID userId, String favoritesTimestamp, UUID songUuid) |
+| **albums_by_user** | Observable<Void> addOrUpdateAlbumsByUser(AlbumsByUserDao) |
+| | Observable<AlbumsByUserDao> getAlbumsByUser(UUID userId, String favoritesTimestamp, UUID albumUuid) |
+| | Observable<ResultSet> getAlbumsByUser(UUID userId, Optional<String> favoritesTimestamp, Optional<Integer> limit) |
+| | Observable<Result<AlbumsByUserDao>> map(Observable<ResultSet>) |
+| | Observable<Void> deleteAlbumsByUser(UUID userId, String favoritesTimestamp, UUID albumUuid) |
+| **artists_by_user** | Observable<Void> addOrUpdateArtistsByUser(ArtistsByUserDao) |
+| | Observable<ArtistsByUserDao> getArtistsByUser(UUID userId, String favoritesTimestamp, UUID artistUuid) |
+| | Observable<ResultSet> getArtistsByUser(UUID userId, Optional<String> favoritesTimestamp, Optional<Integer> limit) |
+| | Observable<Result<ArtistsByUserDao>> map(Observable<ResultSet>) |
+| | Observable<Void> deleteArtistsByUser(UUID userId, String favoritesTimestamp, UUID artistUuid) |
+
+## Packaging & Installation
+
+```bash 
+mvn clean package && mvn -P install compile
+```
---
+++ src/main/java/com/kenzan/msl/account/client/cassandra/query/SongsByUserQuery.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2015, Kenzan, All rights reserved.
+ */
+package com.kenzan.msl.account.client.cassandra.query;
+
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.mapping.MappingManager;
+import com.datastax.driver.mapping.Result;
+import com.google.common.base.Optional;
+import com.kenzan.msl.account.client.dao.SongsByUserDao;
+import com.kenzan.msl.account.client.cassandra.QueryAccessor;
+
+import javax.management.RuntimeErrorException;
+import java.util.Date;
+import java.util.UUID;
+
+public class SongsByUserQuery {
+
+    /**
+     * Retrieves a song from a user library given a userId, songId and a timestamp
+     *
+     * @param queryAccessor QueryAccessor
+     * @param manager com.datastax.driver.mapping.MappingManager
+     * @param userId java.util.UUID
+     * @param timestamp String
+     * @param songUuid java.util.UUID
+     * @return SongsByUserDao
+     */
+    public static SongsByUserDao get(final QueryAccessor queryAccessor, final MappingManager manager,
+                                     final UUID userId, final String timestamp, final UUID songUuid) {
+        Result<SongsByUserDao> results = manager.mapper(SongsByUserDao.class)
+            .map(queryAccessor.songsByUser(userId, new Date(Long.parseLong(timestamp)), songUuid));
+        return results.one();
+    }
+
+    /**
+     * Retrieves a resultSet of songs from a user library given a userId and optionally a timestamp
+     * and limit of results
+     *
+     * @param queryAccessor QueryAccessor
+     * @param userId java.util.UUID
+     * @param timestamp String
+     * @param limit Integer
+     * @return com.datastax.driver.core.ResultSet
+     */
+    public static ResultSet get(final QueryAccessor queryAccessor, final UUID userId, final Optional<String> timestamp,
+                                final Optional<Integer> limit) {
+        if ( limit.isPresent() && timestamp.isPresent() ) {
+            return queryAccessor.songsByUser(userId, new Date(Long.parseLong(timestamp.get())), limit.get());
+        }
+        else if ( limit.isPresent() ) {
+            return queryAccessor.songsByUser(userId, limit.get());
+        }
+        else if ( timestamp.isPresent() ) {
+            return queryAccessor.songsByUser(userId, new Date(Long.parseLong(timestamp.get())));
+        }
+        else {
+            return queryAccessor.songsByUser(userId);
+        }
+    }
+
+    /**
+     * Adds a song to a given userId library
+     *
+     * @param queryAccessor QueryAccessor
+     * @param song SongsByUserDao
+     */
+    public static void add(final QueryAccessor queryAccessor, final SongsByUserDao song) {
+        try {
+            queryAccessor.addLibrarySong(song.getUserId(), "Song", new Date(), song.getSongId(), song.getSongName(),
+                                         song.getSongDuration(), song.getAlbumId(), song.getAlbumName(),
+                                         song.getAlbumYear(), song.getArtistId(), song.getArtistMbid(),
+                                         song.getArtistName());
+        }
+        catch ( Exception error ) {
+            throw error;
+        }
+    }
+
+    /**
+     * Removes a given song from a user library given a userId, the songId and the song timestamp
+     *
+     * @param queryAccessor QueryAccessor
+     * @param userId userId java.util.UUID
+     * @param timestamp java.util.Date
+     * @param songUuid userId java.util.UUID
+     */
+    public static void remove(final QueryAccessor queryAccessor, final UUID userId, final Date timestamp,
+                              final UUID songUuid) {
+        try {
+            queryAccessor.deleteLibrarySong(songUuid, timestamp, userId);
+        }
+        catch ( RuntimeErrorException err ) {
+            throw err;
+        }
+    }
+}
