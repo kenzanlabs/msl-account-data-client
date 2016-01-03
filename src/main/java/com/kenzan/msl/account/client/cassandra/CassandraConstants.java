62eb9f027f1931d8de554a26d6d1f91f56e32584
anram88 <aramirez@kenzanmedia.com>
2016-01-03 12:46:07 -0600

    Initial commit

---
+++ src/main/java/com/kenzan/msl/account/client/cassandra/CassandraConstants.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright 2015, Charter Communications, All rights reserved.
+ */
+package com.kenzan.msl.account.client.cassandra;
+
+/**
+ * @author billschwanitz
+ */
+public class CassandraConstants {
+
+    public static final String MSL_KEYSPACE = "msl";
+}
