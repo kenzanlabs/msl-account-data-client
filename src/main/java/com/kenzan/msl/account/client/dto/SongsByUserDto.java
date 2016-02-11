be36835b0021599bba7acd537d18d14e2c46db4a
Emmanuel <emmanuel-ms@hotmail.com>
2016-02-11 16:30:11 -0600

    MSL-445 Fixed DTO hierarchy

--- src/main/java/com/kenzan/msl/account/client/dto/SongsByUserDto.java
+++ src/main/java/com/kenzan/msl/account/client/dto/SongsByUserDto.java
@@ -6,12 +6,13 @@ package com.kenzan.msl.account.client.dto;
 import com.datastax.driver.mapping.annotations.Column;
 import com.datastax.driver.mapping.annotations.PartitionKey;
 import com.datastax.driver.mapping.annotations.Table;
+import com.kenzan.msl.common.dto.AbstractSongDto;
 
 import java.util.Date;
 import java.util.UUID;
 
 @Table(name = "songs_by_user")
-public class SongsByUserDto {
+public class SongsByUserDto extends AbstractSongDto {
 
     @PartitionKey(value = 0)
     @Column(name = "user_id")
