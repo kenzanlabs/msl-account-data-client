4420a1256154125bf109ac2ea253610db7dc6ba6
anram88 <aramirez@kenzanmedia.com>
2016-03-01 18:43:21 -0600

    MSL-376: implement java code style

--- src/main/java/com/kenzan/msl/account/client/cassandra/query/UserQuery.java
+++ src/main/java/com/kenzan/msl/account/client/cassandra/query/UserQuery.java
@@ -8,41 +8,42 @@ import com.kenzan.msl.account.client.cassandra.QueryAccessor;
 
 public class UserQuery {
 
-    /**
-     * Adds a user
-     *
-     * @param queryAccessor QueryAccessor
-     * @param user String
-     */
-    public static void add(final QueryAccessor queryAccessor, final UserDto user) {
-        queryAccessor.addUser(user.getUsername(), user.getCreationTimestamp(), user.getPassword(), user.getUserId());
-    }
+  /**
+   * Adds a user
+   *
+   * @param queryAccessor QueryAccessor
+   * @param user String
+   */
+  public static void add(final QueryAccessor queryAccessor, final UserDto user) {
+    queryAccessor.addUser(user.getUsername(), user.getCreationTimestamp(), user.getPassword(),
+        user.getUserId());
+  }
 
-    /**
-     * Retrieves a user by username
-     *
-     * @param queryAccessor QueryAccessor
-     * @param manager MappingManager
-     * @param username String
-     * @return UserDto
-     */
-    public static Optional<UserDto> get(final QueryAccessor queryAccessor, final MappingManager manager,
-                                        final String username) {
-        Result<UserDto> results = manager.mapper(UserDto.class).map(queryAccessor.getUser(username));
-        if ( results != null ) {
-            return Optional.of(results.one());
-        }
-        return Optional.absent();
+  /**
+   * Retrieves a user by username
+   *
+   * @param queryAccessor QueryAccessor
+   * @param manager MappingManager
+   * @param username String
+   * @return UserDto
+   */
+  public static Optional<UserDto> get(final QueryAccessor queryAccessor,
+      final MappingManager manager, final String username) {
+    Result<UserDto> results = manager.mapper(UserDto.class).map(queryAccessor.getUser(username));
+    if (results != null) {
+      return Optional.of(results.one());
     }
+    return Optional.absent();
+  }
 
-    /**
-     * Deletes a user by username
-     *
-     * @param queryAccessor QueryAccessor
-     * @param username String
-     */
-    public static void remove(final QueryAccessor queryAccessor, final String username) {
-        queryAccessor.deleteUser(username);
-    }
+  /**
+   * Deletes a user by username
+   *
+   * @param queryAccessor QueryAccessor
+   * @param username String
+   */
+  public static void remove(final QueryAccessor queryAccessor, final String username) {
+    queryAccessor.deleteUser(username);
+  }
 
 }
